a21159514b9304258c1ec39cb7599bf4afa44ddb
gaotianyu1350 <gaotianyu1350@126.com>
2019-04-13 23:59:12 +0800

    add gpio & ioctl test

--- biscuit/c/audio_hello.c
+++ biscuit/c/audio_hello.c
@@ -9,19 +9,37 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/time.h>
+#include <sys/ioctl.h>
 
 int main(int argc, char **argv)
 {
 	printf("hello audio world!\n");
     int fd;
-    fprintf(stderr, "test on gpio (stderr -> Audio)");
+    // fprintf(stderr, "test on gpio (stderr -> Audio)");
+
     fd = open("/dev/stdout", O_WRONLY);
     if (fd < 0) {
         fprintf(stdout, "fail to open /dev/stdout");
         return 1;
     }
-
     write(fd, "write through device file!\n", 27); 
+
+    int fd_gpio;
+    fd_gpio = open("/dev/gpio", O_WRONLY);
+    if (fd_gpio < 0) {
+        fprintf(stdout, "fail to open /dev/gpio");
+        return 1;
+    }
+    if (ioctl(fd_gpio, 10, NULL) != 0) {
+        fprintf(stdout, "fail to use ioctl on /dev/gpio");
+        return 1;
+    }
+    write(fd_gpio, "1", 1);
+    if (ioctl(fd_gpio, 20, NULL) != 0) {
+        fprintf(stdout, "fail to use ioctl on /dev/gpio");
+        return 1;
+    }
+    write(fd_gpio, "1", 1);
 
 	return 0;
 }
